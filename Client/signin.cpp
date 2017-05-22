6845114d7b7aaa9f7aba969d4cca5868d430d5f1
kimjunyeong <robot825@naver.com>
2017-05-22 15:48:32 +0900

    로그인 기능 추가 및 수정

---
+++ Client/signin.cpp
@@ -0,0 +1,14 @@
+#include "signin.h"
+#include "signin.h"
+
+Signin::Signin(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Signin)
+{
+    ui->setupUi(this);
+}
+
+Signin::~Signin()
+{
+    delete ui;
+}
