50704bf7cc9d931247f6d8603b1c98a13b5a90ad
kimjunyeong <robot825@naver.com>
2017-06-04 21:20:00 +0900

    db에서 gender 꺼내옴

--- Server/server.cpp
+++ Server/server.cpp
@@ -1,6 +1,7 @@
 #include "server.h"
 #include <QString>
 #include <QRegExp>
+#include <QSqlRecord>
 #define Path_to_DB "/home/kim/git/QtMultiChat/db/ddj.db"  //db path
 Server::Server(QObject* parent) : QObject(parent) {
     this->crypto.setKey(0x0c2ad4a4acb9f023);
@@ -109,8 +110,14 @@ void Server::onReadyRead() {
             QString userEnPW = loginRex.cap(2);
             QString userPW = crypto.decryptToString(userEnPW);
             QString query="SELECT ID,PW FROM usrInfo WHERE ID=\'"+userID+"\'AND PW=\'"+userPW+"\'";
+            QString query2="SELECT Gender FROM usrInfo";
             if(q.exec(query)){
                 if(q.next()){
+                    q.exec(query2);
+                    if(q.next()) {
+                        QSqlRecord record = q.record();
+                        qDebug() << "gender : " << record.value(1).toInt();
+                    }
                 userInfo temp;
                 temp=clients[socket];
                 temp.userName=userID;
